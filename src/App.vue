6629c3dedd9ea62706cb935824688b7efb366276
jepson <jepsonpp@qq.com>
2019-07-20 18:31:41 +0800

    登录-表单组件分析

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,27 @@
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      msg: '哈哈'
+    }
+  }
+}
+
+</script>
+
 <style lang="scss">
 // scss 定义变量的方式
-$bgColor: #2d434c;
+// $bgColor: #2d434c;
+$bgColor: #fff;
 * {
   margin: 0;
   padding: 0;
   list-style: none;
 }
 body {
-  background: #2d434c;
+  background: $bgColor;
 }
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="login">
-    <el-button>我是按钮</el-button>
-    <el-button type="success" plain>我是按钮</el-button>
-    <el-button type="danger" plain round>我是按钮</el-button>
-    <el-button type="primary" icon="el-icon-search">搜索</el-button>
-    <el-button type="primary" :loading="true">加载中</el-button>
+    <!--
+      el-form: 表单组件
+        :model="form"  model 传入了一个对象, 作用: 给表单元素提供双向数据绑定
+        label-width="80px"
+
+      el-form-item: 表单项, 一个表单可以有多个表单项
+        label 表单项的标签
+
+      el-input: 文本框
+        v-model="form.name" 和传入的form对象的某个属性双向绑定
+     -->
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
