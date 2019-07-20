d7948b3073973facc2ef6aa493ba27e0368ced26
jepson <jepsonpp@qq.com>
2019-07-20 17:32:34 +0800

    elementui 导入

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    <el-button>我是按钮</el-button>
+    <el-button type="success" plain>我是按钮</el-button>
+    <el-button type="danger" plain round>我是按钮</el-button>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <el-button type="primary" :loading="true">加载中</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
