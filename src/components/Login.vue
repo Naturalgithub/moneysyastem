5ef21e02d6d68f271a10f0569f697b0996e5f1be
jepson <jepsonpp@qq.com>
2019-07-20 23:23:37 +0800

    登录功能-发送ajax请求

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -28,6 +28,8 @@
 </template>
 
 <script>
+// 导入 axios
+import axios from 'axios'
 export default {
   data () {
     return {
@@ -37,12 +39,12 @@ export default {
       },
       rules: {
         username: [
-          { required: true, message: '请输入用户名', trigger: 'blur' },
-          { min: 5, max: 12, message: '用户名长度在 5 到 12 个字符', trigger: 'blur' }
+          { required: true, message: '请输入用户名', trigger: 'change' },
+          { min: 5, max: 12, message: '用户名长度在 5 到 12 个字符', trigger: 'change' }
         ],
         password: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 5, max: 12, message: '密码长度在 3 到 12 个字符', trigger: 'blur' }
+          { required: true, message: '请输入密码', trigger: 'change' },
+          { min: 5, max: 12, message: '密码长度在 3 到 12 个字符', trigger: 'change' }
         ]
       }
     }
@@ -52,6 +54,19 @@ export default {
       this.$refs.form.validate(valid => {
         if (valid) {
           // 校验成功, 发送ajax
+          axios({
+            method: 'post',
+            url: 'http://localhost:8888/api/private/v1/login',
+            data: this.form
+          }).then(res => {
+            console.log(res.data)
+            const { meta } = res.data
+            if (meta.status === 200) {
+              console.log('登录成功')
+            } else {
+              console.log(meta.msg)
+            }
+          })
         } else {
           // 校验失败
           return false
