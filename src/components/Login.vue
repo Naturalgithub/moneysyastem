74fee976b65bd1c9a3cda34026b71164a09f3caf
jepson <jepsonpp@qq.com>
2019-07-24 00:12:15 +0800

    修复首页回车登录的功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -14,13 +14,13 @@
     <el-form ref="form" status-icon :model="form" :rules="rules" label-width="80px">
       <img src="../assets/avatar.jpg" alt="">
       <el-form-item label="用户名" prop="username">
-        <el-input v-model="form.username"  placeholder="请输入用户名"></el-input>
+        <el-input @keyup.enter.native="login" v-model="form.username"  placeholder="请输入用户名"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
+        <el-input @keyup.enter.native="login" v-model="form.password" type="password" placeholder="请输入密码"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm">登录</el-button>
+        <el-button type="primary" @click="login">登录</el-button>
         <el-button @click="resetForm">重置</el-button>
       </el-form-item>
     </el-form>
@@ -50,7 +50,7 @@ export default {
     }
   },
   methods: {
-    submitForm () {
+    login () {
       this.$refs.form.validate(valid => {
         // 如果校验不通过, 直接返回
         if (!valid) return
